26cd25465db377a1d5f7989c624628038798702c
Sal <salvador.banuelos.424@gmail.com>
2022-07-27 15:52:55 -0700

    update

--- react-app/src/pages/Portfolio.jsx
+++ react-app/src/pages/Portfolio.jsx
@@ -6,6 +6,7 @@ import Port3 from '../assets/3run.jpg';
 import Port4 from '../assets/4hor.jpg';
 import Port5 from '../assets/5duty.png';
 import Port6 from '../assets/6pass.png';
+import newport from '../assets/school.jpg';
 import { FaGithub, FaLink } from 'react-icons/fa';
 
 
@@ -14,6 +15,13 @@ function Portfolio() {
   return (
     <div className='w-full h-full'>
       <div className='flex-wrap'>
+        
+      <PortfolioItem className='flex-auto' name='School Website' image={newport}/>
+        <h1 className='flex font-bold bg-sky-200 w-full mx-4 p-2 text-center rounded-b-3xl text-slate-700 text-3xl'>-REACT -TAILWING -JAVASCRIPT
+        <a href='https://github.com/SalvadorBanuelos424/React_CV'  ><FaGithub className='text-6xl m-4 hover:text-sky-400'/></a>
+        <a  href='https://salvadorbanuelos424.github.io/React_CV/'  ><FaLink className='text-6xl m-4 hover:text-sky-400 '/></a>
+        </h1>
+
         <PortfolioItem className='flex-auto' name='Park View' image={Port1}/>
         <h1 className='flex font-bold bg-sky-200 w-full mx-4 p-2 text-center rounded-b-3xl text-slate-700 text-3xl'>-HANDLEBARS -CSS -JAVASCRIPT
         <a href='https://github.com/SalvadorBanuelos424/Park-Place-master-planned-community'  ><FaGithub className='text-6xl m-4 hover:text-sky-400'/></a>
